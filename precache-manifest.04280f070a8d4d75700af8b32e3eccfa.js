6092c3dc1f9d323a765853a7e734ce3ee75ac41f
lucaspdude <lucaspdude@gmail.com>
2019-12-11 00:31:10 -0300

    Updates

---
+++ precache-manifest.04280f070a8d4d75700af8b32e3eccfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde514b2001a0fb81c8e9350ab1b17f7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8edc6d7b7649ae279f3d",
+    "url": "/monsters-rolodex/static/css/main.035bb9e1.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8edc6d7b7649ae279f3d",
+    "url": "/monsters-rolodex/static/js/main.9a2182bb.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
